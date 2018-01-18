c832e282d88d7d4381f1a110a58bb2a847246e5f
rushikesh <35166569+rushimanjrekar@users.noreply.github.com>
2018-01-18 15:38:46 +0530

    used namespace std

--- ATOM.cpp
+++ ATOM.cpp
@@ -1,4 +1,5 @@
 #include<cstdio>
+using namespace std;
 
 double n,k,m;
 
@@ -12,20 +13,20 @@ void solve()
         k = r*k;
         ans++;
     }
-    //cout << ans << endl;
+    cout << ans << endl;
     printf("%d\n",ans);
 }
 
 int main()
 {
-//    ios::sync_with_stdio(false);
+   ios::sync_with_stdio(false);
 
     int t;
-    //cin >> t;
+    cin >> t;
     scanf("%d",&t);
     while(t--)
     {
-        //cin >> n >> k >> m;
+        cin >> n >> k >> m;
         scanf("%lf%lf%lf",&n,&k,&m);
         solve();
     }
